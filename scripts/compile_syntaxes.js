20acd8f6d430102fbbc255664fc4dd6bf5b5f8ad
猴貓 <a102009102009@gmail.com>
2023-05-22 18:32:40 +0800

    fix run script

--- scripts/compile_syntaxes.js
+++ scripts/compile_syntaxes.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 
 try {
   fs.readdirSync('syntaxes').forEach((file) => {
-    const doc = yaml.load(fs.readFileSync(file, 'utf8'));
+    file = `syntaxes/${file}`;
+
     fs.writeFileSync(
       file.replace(/.yaml$/, '.json'),
-      JSON.stringify(doc, null, 2)
+      JSON.stringify(yaml.load(fs.readFileSync(file, 'utf8')), null, 2)
     );
   });
 } catch (e) {
