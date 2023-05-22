b56cb54e1d5d5d5e03d0b56791a310b4376a755b
猴貓 <a102009102009@gmail.com>
2023-05-22 18:44:34 +0800

    fix watch script

--- scripts/compile_syntaxes.js
+++ scripts/compile_syntaxes.js
@@ -3,11 +3,13 @@ const fs = require('fs');
 
 try {
   fs.readdirSync('syntaxes').forEach((file) => {
+    if (!/\.(yaml|yml)$/.test(file)) return;
+
     file = `syntaxes/${file}`;
 
     fs.writeFileSync(
       file.replace(/.yaml$/, '.json'),
-      JSON.stringify(yaml.load(fs.readFileSync(file, 'utf8')), null, 2)
+      JSON.stringify(yaml.load(fs.readFileSync(file, 'utf8')))
     );
   });
 } catch (e) {
